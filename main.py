223213b93a686a82eb49d1a676a83c44b5220349
Ghafoor Nazari <GhafoorNazari@proton.me>
2025-01-23 22:28:55 +0100

    added main file

---
+++ main.py
@@ -0,0 +1,68 @@
+import requests, re
+from requests.structures import CaseInsensitiveDict
+
+url = "http://www.insecam.org/en/jsoncountries/"
+
+headers = CaseInsensitiveDict()
+headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
+headers["Cache-Control"] = "max-age=0"
+headers["Connection"] = "keep-alive"
+headers["Host"] = "www.insecam.org"
+headers["Upgrade-Insecure-Requests"] = "1"
+headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+
+print("""
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠉⠀⠀⠀⠀⠈⠙⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠦⠀⠀⠀⠀⠀⠀⠠⠾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿
+                                @NotaGlitch""")
+
+try:
+    resp = requests.get(url, headers=headers)
+    if resp.status_code == 200:
+        data = resp.json()
+        countries = data['countries']
+
+        for key, value in countries.items():
+            print(f'Code : ({key}) - {value["country"]} / ({value["count"]})')
+
+        country = input("Code(##) : ")
+        res = requests.get(f"http://www.insecam.org/en/bycountry/{country}", headers=headers)
+        if res.status_code == 200:
+            last_page = re.findall(r'pagenavigator\("\?page=", (\d+)', res.text)
+            if last_page:
+                last_page = last_page[0]
+
+                for page in range(int(last_page)):
+                    res = requests.get(f"http://www.insecam.org/en/bycountry/{country}/?page={page}", headers=headers)
+                    if res.status_code == 200:
+                        find_ip = re.findall(r"http://\d+.\d+.\d+.\d+:\d+", res.text)
+
+                        # Open the file in append mode ('a')
+                        with open(f'{country}.txt', 'a') as f:  # Changed from 'w' to 'a'
+                            for ip in find_ip:
+                                print("\033[1;31m", ip)
+                                f.write(f'{ip}\n')  # Appending to the file
+            else:
+                print("No pages found.")
+        else:
+            print(f"Failed to get data for country code: {country}")
+    else:
+        print("Failed to get country list data.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+finally:
+    print("\033[1;37m")
+    print(f'Save File: {country}.txt')
+    exit()
